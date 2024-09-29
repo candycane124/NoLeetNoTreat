af96e51ef88144ed3078d8359ce49130ffaf64ae
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-09-29 00:47:36 -0400

    modified some files

--- popup.js
+++ popup.js
@@ -48,7 +48,7 @@ document.getElementById('linkAccount').addEventListener('click', async () => {
         const userData = await fetchLeetCodeUserData(username);
         console.log('User data:', userData);
 
-        if (userData || username !== null) {
+        if (userData) {
             // Update UI based on fetched data
             document.getElementById('login').innerHTML = '';
             document.getElementById('login-status').innerHTML = `
